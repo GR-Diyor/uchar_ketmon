6065259740474a3009b362cbe77a3eb42f5ee74d
Diyorbek Nizomiddinov <93899421+Diyorbek0617@users.noreply.github.com>
2023-07-04 09:04:57 +0500

    any changed

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -6,78 +6,75 @@ import 'package:uchar_ketmon/view/widget/app_screen/full_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
-  static const String id="Home";
+  static const String id = "Home";
 
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  static double yAxis=0;
+  static double yAxis = 0;
   double time = 0;
   double height = 0;
   double initialHeight = yAxis;
   bool gamehasStarted = false;
 
-
-
   jump() {
-setState(() {
-time =0;
-initialHeight = yAxis;
-});
-
+    setState(() {
+      time = 0;
+      initialHeight = yAxis;
+    });
   }
 
-  start(){
+  start() {
     gamehasStarted = true;
     Timer.periodic(const Duration(milliseconds: 50), (timer) {
-      time +=0.05;
-      height = -4.9*time*time+2.8*time;
+      time += 0.05;
+      height = -4.9 * time * time + 2.8 * time;
       setState(() {
-        yAxis-=initialHeight - height;
+        yAxis -= initialHeight - height;
       });
-      if(yAxis>1){
+      if (yAxis > 1) {
         timer.cancel();
-          gamehasStarted = false;
+        gamehasStarted = false;
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
-    return fullScreen(context,
+    return fullScreen(
+        context,
         Column(
           children: [
             Expanded(
                 flex: 2,
                 child: GestureDetector(
-                  onTap: (){
-                    if(gamehasStarted){
+                  onTap: () {
+                    if (gamehasStarted) {
                       jump();
-                    }else{
+                    } else {
                       start();
                     }
                   },
                   child: AnimatedContainer(
                     color: AppColor.blue,
-                    alignment:  Alignment(0,yAxis),
+                    alignment: Alignment(0, yAxis),
                     duration: const Duration(milliseconds: 0),
-                  child: const FlutterLogo(size: 40,),
-
+                    child: const FlutterLogo(
+                      size: 40,
+                    ),
                   ),
-                )
-            ),
-            Container(color: AppColor.green,
-            height: 15,
+                )),
+            Container(
+              color: AppColor.green,
+              height: 15,
             ),
             Expanded(
                 child: Container(
-                  color: AppColor.brown,)
-            )
+              color: AppColor.brown,
+            ))
           ],
-        )
-    );
+        ));
   }
-
-
 }
